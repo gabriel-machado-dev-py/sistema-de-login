ffec73cd6ed3ef0e95d6ca10c2fadda3bca52f6d
MachadoProgrammer <gabrieldiasmachado@gmail.com>
2024-08-13 11:10:44 -0300

    adicionado a tela de cadastro

--- README.md
+++ README.md
@@ -7,9 +7,17 @@ Este é um projeto em python criado para ser um **sistema de login** para regist
 ## Funcionalidades ⚙️
 
 1. **Registro de usuários:**
-   * Permite que novos usuários se registrem com um nome de usuário e senha
-   * Armazenar o nome de usuário e a senha criptografada no banco de dados
-     SQLite.
 
+   * Permite que novos usuários se registrem com um nome de usuário, email e senha
+   * Permite que o usuário veja sua senha
+   * Armazena o nome de usuário e a senha criptografada no banco de dados
+     SQLite.
 2. **Login de Usuários:**
-   * 
+
+   * Permite faça login com suas credenciais (nome de usuário e
+     senha).
+   * Verifica as credenciais no banco de dados SQLite.
+   * Permite que o usuário veja sua senha
+3. **Banco de Dados:**
+
+   * Integrado com criação e validação de usuário e senha
--- graphic_interface.py
+++ graphic_interface.py
@@ -8,9 +8,11 @@ class App(tk.CTk):
         self.configuracao_da_janela_inicial()
         self.tela_de_login()
         
+        
     """
     Configuração da janela principal
     """
+        
     def configuracao_da_janela_inicial(self):
         self.title("Sistema de Login")
         self.geometry("720x400")
@@ -21,6 +23,8 @@ class App(tk.CTk):
     
     1. Configuração da imagem de fundo
     2. Configuração do formulário de login
+        1. Campo de usuario e senha
+        2. Botões de login e cadastro
     """
     def tela_de_login(self):
       # Configuração da imagem de fundo
@@ -60,11 +64,59 @@ class App(tk.CTk):
       # Botões de ação - Login e Cadastro
       self.span = tk.CTkLabel(self.frame_login, text="Não tem uma conta? Clique aqui para se cadastrar", font=("Century Gothic bold", 12))
       self.span.grid(row=5, column=0, padx=10, pady=10)
-        
-      self.btn_signup = tk.CTkButton(self.frame_login, width=30, text="Cadastre-se".upper(), font=("Century Gothic bold", 14, "bold"), corner_radius=15, fg_color="#099", hover_color="#099666")
+      
+      # Botão cadastro
+      self.btn_signup = tk.CTkButton(self.frame_login, width=30, text="Cadastre-se".upper(), font=("Century Gothic bold", 14, "bold"), corner_radius=15, fg_color="#099", hover_color="#099666", command=self.tela_de_cadastro)
       self.btn_signup.grid(row=6, column=0, padx=10, pady=10)
+      
+    def tela_de_cadastro(self):
+        """
+        Configuração da tela de cadastro
+        1. Campo de usuario
+        2. Campo de email
+        3. Campo de senha e confirmar senha
+        """
+        # Remover o formulário de login 
+        self.frame_login.place_forget()
         
-
+        # Frame de formulario de cadastro
+        self.frame_cadastro = tk.CTkFrame(self, width=350, height=380)
+        self.frame_cadastro.place(x=350, y=10)
+        
+        # Criar os widgets da tela de cadastro
+        self.lb_title = tk.CTkLabel(self.frame_cadastro, text="Faça seu cadastro", font=("Century Gothic", 22, "bold"))
+        self.lb_title.grid(row=0, column=0, padx=10, pady=5)
+        
+        # campo usuario
+        self.user_name_cadastro = tk.CTkEntry(self.frame_cadastro, width=300, placeholder_text="Digite seu nome de usuário", font=("Century Gothic bold", 16, "bold"), corner_radius=15)
+        self.user_name_cadastro.grid(row=1, column=0, padx=10, pady=5)
+        # confirmar usuario
+        self.user_name_cadastro = tk.CTkEntry(self.frame_cadastro, width=300, placeholder_text="Digite seu email", font=("Century Gothic bold", 16, "bold"), corner_radius=15)
+        self.user_name_cadastro.grid(row=2, column=0, padx=10, pady=5)
+        
+        # Campo Senha
+        self.password_cadastro = tk.CTkEntry(self.frame_cadastro, width=300, placeholder_text="Digite sua senha", font=("Century Gothic bold", 16, "bold"), show="*", corner_radius=15)
+        self.password_cadastro.grid(row=3, column=0, padx=10, pady=5)
+        # confirma senha
+        self.confirm_password_cadastro = tk.CTkEntry(self.frame_cadastro, width=300, placeholder_text="Confirme sua senha", font=("Century Gothic bold", 16, "bold"), show="*", corner_radius=15)
+        self.confirm_password_cadastro.grid(row=4, column=0, padx=10, pady=5)
+        # mostrar senha
+        self.show_password = tk.CTkCheckBox(self.frame_cadastro, text="Clique para ver a senha", font=("Century Gothic", 12))
+        self.show_password.grid(row=5, column=0, padx=10, pady=5)
+        
+        # Fazer cadastro
+        self.btn_signup_user = tk.CTkButton(self.frame_cadastro, width=300, text="Cadastre-se".upper(), font=("Century Gothic bold", 14, "bold"), corner_radius=15, fg_color="#099", hover_color="#099666", command=self.tela_de_cadastro)
+        self.btn_signup_user.grid(row=6, column=0, padx=10, pady=5)
+        
+        # voltar a tela de login
+        self.btn_login_back = tk.CTkButton(self.frame_cadastro, width=300, text="Voltar a tela de login".upper(), font=("Century Gothic bold", 12, "bold"), corner_radius=15, fg_color="#9370DB", command=self.tela_de_login)
+        self.btn_login_back.grid(row=7, column=0, padx=10, pady=5)
+        
+        
+        
+        
+        
+        
 if __name__ == "__main__":
     app = App()
     app.mainloop()
