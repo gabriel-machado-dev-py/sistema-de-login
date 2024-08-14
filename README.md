0bc0fd6a512bf2fd721e09617036406f5e13d0c2
MachadoProgrammer <gabrieldiasmachado@gmail.com>
2024-08-14 19:01:26 -0300

    adicionado verificacao de cadastro

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## Descrição do projeto 📋
 
-Este é um projeto em python criado para ser um **sistema de login** para registro de usuários.
+Este é um projeto em python criado para ser um **Sistema de login** para registro de usuários.
 
 ## Funcionalidades ⚙️
 
@@ -10,14 +10,62 @@ Este é um projeto em python criado para ser um **sistema de login** para regist
 
    * Permite que novos usuários se registrem com um nome de usuário, email e senha
    * Permite que o usuário veja sua senha
-   * Armazena o nome de usuário e a senha criptografada no banco de dados
-     SQLite.
+   * Armazena o nome de usuário e a senha criptografada no banco de dados SQLite.
+
 2. **Login de Usuários:**
 
    * Permite faça login com suas credenciais (nome de usuário e
      senha).
    * Verifica as credenciais no banco de dados SQLite.
    * Permite que o usuário veja sua senha
+   
 3. **Banco de Dados:**
 
    * Integrado com criação e validação de usuário e senha
+
+## Estrutura de pastas 🧱
+
+/graphic_interface.py  # Código-fonte principal da aplicação 
+/astronauta.png        # Imagem da aplicação
+/requirements.txt      # Tecnologias hospedadas para instalação
+
+
+# Tecnologias utilizadas 💻
+
+- **customtkinter**: Ambiente para criação da interface gráfica
+- **tkinter**: Usado para algumas funcionalidades no projeto como: messagebox
+- **sqlite3**: Banco de dados usado no projeto
+
+## Configuração para Desenvolvedores 🔧
+
+### Requisitos
+
+- Python 3.x 🐍
+- Bibliotecas: `customtkinter`, `tkinter`, `sqlite3`
+
+### Instalação das Dependências
+
+1. Clone o repositório:
+
+   ```bash
+   git clone https://github.com/gabriel-machado-dev-py/sistema-de-login.git
+   ```
+   
+2. Configure um ambiente virtual (opcional, mas recomendado):
+
+   ```bash
+   python -m venv venv
+   # No Linux use `source venv/bin/activate`
+   # No Windows use `venv\Scripts\activate`
+   ```
+
+3. Instale as dependências usando o arquivo requirements.txt:
+
+   ```bash
+   pip install -r requirements.txt
+   
+
+## Licença 📝
+
+Este projeto está licenciado sob a Licença MIT. Consulte o arquivo `LICENSE` para mais detalhes.
+   ```
--- graphic_interface.py
+++ graphic_interface.py
@@ -3,24 +3,24 @@ from tkinter import *
 from tkinter import messagebox
 import sqlite3
 
-
+                                                                                            
 class BackEnd():
     """
     Configuração do banco de dados
     1. Conexão com o banco de dados
-    2. Desconexão do banco de dados
-    3. Criação da tabela
+    2. Desconexão do banco de dados 
+    3. Criação da tabela de usuários
     4. Cadastro de usuários
     """
-    def conect_db(self):
+    def conect_db(self) -> None:
         self.conn = sqlite3.connect("Sistema_de_login.db")
         self.cursor = self.conn.cursor()
          
-    def desconect_db(self):
+    def desconect_db(self) -> None:
         self.conn.close()
         print("Banco de dados desconectado")
         
-    def criar_tabela(self):
+    def criar_tabela(self) -> None:
         self.conect_db()
         
         self.cursor.execute("""
@@ -36,7 +36,7 @@ class BackEnd():
         print("Tabela criada com sucesso")
         self.desconect_db()
     
-    def cadastrar_usario(self):
+    def cadastrar_usario(self) -> None:
         self.username_entry = self.user_name_cadastro.get()
         self.email_entry = self.user_email_cadastro.get()
         self.password_entry = self.password_cadastro.get()
@@ -64,12 +64,39 @@ class BackEnd():
             else:
                 self.conn.commit()
                 messagebox.showinfo(title="Sistema de login", message=f"Usuário {self.username_entry} cadastrado!!!")
-                
+                self.desconect_db()
+                self.limpa_campo_cadastro()
         except:
             messagebox.showerror(title="Sistema de login", message="Erro ao cadastrar o usuário, tente novamente.")
+            self.desconect_db()
+
+    def logar_usuario(self) -> None:
+        self.username_entry = self.user_name.get()
+        self.password_entry = self.password.get()
+        
+        self.conect_db()
+        
+        self.cursor.execute("""
+                            SELECT * FROM Usuarios
+                            WHERE user = ? AND password = ?
+                            """, (self.username_entry, self.password_entry))
+        
+        try:
+            if (self.cursor.fetchone() is not None):
+                messagebox.showinfo(title="Sistema de login", message=f"Usuário {self.username_entry} logado com sucesso!")
+                self.desconect_db()
+                self.limpa_campo_login()
+            else:
+                messagebox.showerror(title="Sistema de login", message="Usuário ou senha incorretos!")
+                self.desconect_db()
+                self.limpa_campo_login()
+        except:
+            messagebox.showerror(title="Sistema de login", message="Erro ao logar o usuário, tente novamente.")
+            self.desconect_db()
+            
     
 class App(tk.CTk, BackEnd):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.configuracao_da_janela_inicial()
         self.tela_de_login()  
@@ -78,7 +105,7 @@ class App(tk.CTk, BackEnd):
     """
     Configuração da janela principal
     """  
-    def configuracao_da_janela_inicial(self):
+    def configuracao_da_janela_inicial(self) -> None:
         self.title("Sistema de Login")
         self.geometry("720x400")
         self.resizable(False, False)  
@@ -91,7 +118,8 @@ class App(tk.CTk, BackEnd):
         1. Campo de usuario e senha
         2. Botões de login e cadastro
     """
-    def tela_de_login(self):
+    def tela_de_login(self) -> None:
+        
       # Configuração da imagem de fundo
       self.img = PhotoImage(file="astronauta.png")
       self.img = self.img.subsample(6)
@@ -119,14 +147,15 @@ class App(tk.CTk, BackEnd):
       self.password.grid(row=2, column=0, padx=10, pady=10)
       
       # Mostrar senha
-      self.show_password = tk.CTkCheckBox(self.frame_login, text="Clique para ver a senha", font=("Century Gothic", 12))
+      self.show_password_var = tk.StringVar(value="off")
+      self.show_password = tk.CTkCheckBox(self.frame_login, text="Clique para ver a senha", font=("Century Gothic", 12), variable=self.show_password_var, onvalue="on", offvalue="off", command=self.toggle_password_visibility)
       self.show_password.grid(row=3, column=0, padx=10, pady=10)
       
-      # Botões de ação - Login e Cadastro
-      self.btn_login = tk.CTkButton(self.frame_login, width=300, text="Fazer login".upper(), font=("Century Gothic bold", 16, "bold"), corner_radius=15, fg_color="#9370DB")
+      # Botões de ação - Login
+      self.btn_login = tk.CTkButton(self.frame_login, width=300, text="Fazer login".upper(), font=("Century Gothic bold", 16, "bold"), corner_radius=15, fg_color="#9370DB", command=self.logar_usuario)
       self.btn_login.grid(row=4, column=0, padx=10, pady=10)
       
-      # Botões de ação - Login e Cadastro
+      # Botões de ação - Cadastro
       self.span = tk.CTkLabel(self.frame_login, text="Não tem uma conta? Clique aqui para se cadastrar", font=("Century Gothic bold", 12))
       self.span.grid(row=5, column=0, padx=10, pady=10)
       
@@ -135,13 +164,13 @@ class App(tk.CTk, BackEnd):
       self.btn_signup.grid(row=6, column=0, padx=10, pady=10)
       
     """
-        Configuração da tela de cadastro
-        1. Campo de usuario
-        2. Campo de email
-        3. Campo de senha e confirmar senha
-        4. Campo de voltar a tela de login
-        """  
-    def tela_de_cadastro(self):
+    Configuração da tela de cadastro
+    1. Campo de usuario
+    2. Campo de email
+    3. Campo de senha e confirmar senha
+    4. Campo de voltar a tela de login
+    """  
+    def tela_de_cadastro(self) -> None:
         # Remover o formulário de login 
         self.frame_login.place_forget()
         
@@ -161,37 +190,61 @@ class App(tk.CTk, BackEnd):
         self.user_email_cadastro = tk.CTkEntry(self.frame_cadastro, width=300, placeholder_text="Digite seu email", font=("Century Gothic bold", 16, "bold"), corner_radius=15)
         self.user_email_cadastro.grid(row=2, column=0, padx=10, pady=5)
         
-        # Campo Senha
+        # campo Senha
         self.password_cadastro = tk.CTkEntry(self.frame_cadastro, width=300, placeholder_text="Digite sua senha", font=("Century Gothic bold", 16, "bold"), show="*", corner_radius=15)
         self.password_cadastro.grid(row=3, column=0, padx=10, pady=5)
+        
         # confirma senha
         self.confirm_password_cadastro = tk.CTkEntry(self.frame_cadastro, width=300, placeholder_text="Confirme sua senha", font=("Century Gothic bold", 16, "bold"), show="*", corner_radius=15)
         self.confirm_password_cadastro.grid(row=4, column=0, padx=10, pady=5)
+        
         # mostrar senha
-        self.show_password = tk.CTkCheckBox(self.frame_cadastro, text="Clique para ver a senha", font=("Century Gothic", 12))
+        self.show_password_var = tk.StringVar(value="off")
+        self.show_password = tk.CTkCheckBox(self.frame_cadastro, text="Clique para ver a senha", font=("Century Gothic", 12), variable=self.show_password_var, onvalue="on", offvalue="off", command=self.toggle_password_visibility)
         self.show_password.grid(row=5, column=0, padx=10, pady=5)
         
         # Fazer cadastro
         self.btn_signup_user = tk.CTkButton(self.frame_cadastro, width=300, text="Cadastre-se".upper(), font=("Century Gothic bold", 14, "bold"), corner_radius=15, fg_color="#099", hover_color="#099666", command=self.cadastrar_usario)
         self.btn_signup_user.grid(row=6, column=0, padx=10, pady=5)
         
-        # voltar a tela de login
+        # Voltar a tela de login
         self.btn_login_back = tk.CTkButton(self.frame_cadastro, width=300, text="Voltar a tela de login".upper(), font=("Century Gothic bold", 12, "bold"), corner_radius=15, fg_color="#9370DB", command=self.tela_de_login)
         self.btn_login_back.grid(row=7, column=0, padx=10, pady=5)
     
     """
-    Limpeza dos campos de input da tela de cadastro e login
-    
+    1. Limpeza dos campos de input da tela de cadastro e login
+    2. Mostrar ou esconder a senha
     """    
-    def limpa_campo_cadastro(self):
+    def limpa_campo_cadastro(self) -> None:
         self.user_name_cadastro.delete(0, END)
         self.user_email_cadastro.delete(0, END)
         self.password_cadastro.delete(0, END)
-        self.confirm_password_cadastro.delete(0, END)        
-    def limpa_campo_login(self):
+        self.confirm_password_cadastro.delete(0, END)  
+              
+    def limpa_campo_login(self) -> None:
         self.user_name.delete(0, END)
         self.password.delete(0, END)
+
+    # Mostrar ou esconder a senha da tela de cadastro
+    def toggle_password_visibility(self) -> None:
+        if self.show_password_var.get() == "on":
+            self.password_cadastro.configure(show="")
+            self.confirm_password_cadastro.configure(show="")
+        else:
+            self.password_cadastro.configure(show="*")
+            self.confirm_password_cadastro.configure(show="*")
+
+    # Mostrar ou esconder a senha da tela de login
+    def toggle_password_visibility(self) -> None:
+        if self.show_password_var.get() == "on":
+            self.password.configure(show="")
+        else:
+            self.password.configure(show="*")
        
 if __name__ == "__main__":
     app = App()
     app.mainloop()
+    
+    
+    
+        
